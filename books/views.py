b24e483b1c199e09373839ada3d716b30f77b647
verbik <verbikyullya@gmail.com>
2024-01-17 13:29:17 +0200

    Added custom permission IsAdminOrReadOnly: - only admin users can create/update/delete - all users (even those not authenticated) can list and retrieve books

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 
+from books.permissions import IsAdminOrReadOnly
 from books.serializers import BookSerializer
 from books.models import Book
 
@@ -7,3 +8,4 @@ from books.models import Book
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
