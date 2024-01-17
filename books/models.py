b93554996a98a52e20a73f377c24bf3327c680b2
verbik <verbikyullya@gmail.com>
2024-01-17 12:34:02 +0200

    Added book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "HARD", _("Hard")
+        SOFT = "SOFT", _("Soft")
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=Cover)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
