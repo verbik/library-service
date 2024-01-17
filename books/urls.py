33ae310b35e83d1fa684491aa10197caf523de6b
verbik <verbikyullya@gmail.com>
2024-01-17 13:09:03 +0200

    Implement serializer & views for all endpoints for book service

---
+++ books/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from books.views import BookViewSet
+
+router = routers.DefaultRouter()
+router.register("", BookViewSet)
+
+urlpatterns = router.urls
+
+app_name = "books"
--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.serializers import BookSerializer
+from books.models import Book
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
