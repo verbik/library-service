82ed6ee1a8a3657bd0c674d1078c35155d5e8134
verbik <verbikyullya@gmail.com>
2024-01-17 16:18:54 +0200

    Added Borrowing model with constraints for borrow_date, expected_return_date, actual_return_date

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,25 @@
+from datetime import date
+
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField(
+        validators=[MinValueValidator(limit_value=date.today)]
+    )
+    actual_return_date = models.DateField(
+        null=True, blank=True, validators=[MinValueValidator(limit_value=date.today)]
+    )
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["expected_return_date"]
+
+    def __str__(self):
+        return f"{self.book.title}, expected return date: {self.expected_return_date}."
