07acf32200cdeed8fdd057fd21aa537590556430
verbik <verbikyullya@gmail.com>
2024-01-18 23:26:42 +0200

    Created Notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Notification(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.message[:50]}..."
